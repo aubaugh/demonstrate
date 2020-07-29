5b3f739388a74b64baf1a7e0408f1a1388d354d7
Austin Baugh <austinsbaugh@gmail.com>
2020-07-28 20:46:06 -0700

    Attempting to pass describe properties to descendant blocks

--- src/block.rs
+++ src/block.rs
@@ -1,5 +1,5 @@
 use syn::parse::{Parse, ParseStream, Result};
-use syn::{Attribute, Type, braced, Token, Ident, Stmt};
+use syn::{braced, Attribute, Ident, Stmt, Token, Type};
 
 mod keyword {
     use syn::custom_keyword;
@@ -12,31 +12,6 @@ mod keyword {
     custom_keyword!(test);
 }
 
-#[derive(Clone)]
-pub(crate) enum Block {
-    Describe(Describe),
-    Test(Test),
-}
-
-impl Parse for Block {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let lookahead = input.lookahead1();
-        // TODO: peek past attributes
-
-        if lookahead.peek(Token![async]) {
-            input.parse::<Token![async]>()?;
-        }
-
-        if lookahead.peek(keyword::describe) || lookahead.peek(keyword::context) {
-            Ok(Block::Describe(input.parse::<Describe>()?))
-        } else if lookahead.peek(keyword::it) || lookahead.peek(keyword::test) {
-            Ok(Block::Test(input.parse::<Test>()?))
-        } else {
-            Err(input.error("Not a valid block"))
-        }
-    }
-}
-
 pub(crate) enum ScopeBlock {
     Regular(Block),
     Before(Vec<Stmt>),
@@ -47,18 +22,17 @@ impl Parse for ScopeBlock {
     fn parse(input: ParseStream) -> Result<Self> {
         let lookahead = input.lookahead1();
         let content;
+
         if lookahead.peek(keyword::before) {
             input.parse::<keyword::before>()?;
             braced!(content in input);
-            Ok(ScopeBlock::Before(
-                content.call(syn::Block::parse_within)?
-            ))
+
+            Ok(ScopeBlock::Before(content.call(syn::Block::parse_within)?))
         } else if lookahead.peek(keyword::after) {
             input.parse::<keyword::after>()?;
             braced!(content in input);
-            Ok(ScopeBlock::After(
-                content.call(syn::Block::parse_within)?
-            ))
+
+            Ok(ScopeBlock::After(content.call(syn::Block::parse_within)?))
         } else {
             Ok(ScopeBlock::Regular(input.parse::<Block>()?))
         }
@@ -108,75 +82,43 @@ impl Parse for Scope {
 }
 
 #[derive(Clone)]
-pub(crate) struct Describe {
+pub(crate) enum BlockType {
+    Describe(Scope),
+    Test(Vec<Stmt>),
+}
+
+#[derive(Clone)]
+pub(crate) struct Block {
     pub(crate) attributes: Vec<Attribute>,
     pub(crate) is_async: bool,
     pub(crate) ident: Ident,
     pub(crate) return_type: Option<Type>,
-    pub(crate) scope: Scope,
+    pub(crate) content: BlockType,
 }
 
-impl Parse for Describe {
+impl Parse for Block {
     fn parse(input: ParseStream) -> Result<Self> {
-        let mut is_async = false;
+        let lookahead = input.lookahead1();
 
         let attributes = input.call(Attribute::parse_outer)?;
 
-        let lookahead = input.lookahead1();
+        let mut is_async = false;
         if lookahead.peek(Token![async]) {
             is_async = true;
             input.parse::<Token![async]>()?;
         }
 
+        let mut is_test = false;
         if lookahead.peek(keyword::describe) {
             input.parse::<keyword::describe>()?;
         } else if lookahead.peek(keyword::context) {
             input.parse::<keyword::context>()?;
-        }
-
-        let ident = input.parse::<Ident>()?;
-
-        // TODO: parse return type
-
-        let content;
-        braced!(content in input);
-        let scope = content.parse::<Scope>()?;
-
-        Ok(Describe {
-            attributes,
-            is_async,
-            ident,
-            return_type: None,
-            scope
-        })
-    }
-}
-
-#[derive(Clone)]
-pub(crate) struct Test {
-    pub(crate) attributes: Vec<Attribute>,
-    pub(crate) is_async: bool,
-    pub(crate) ident: Ident,
-    pub(crate) return_type: Option<Type>,
-    pub(crate) block: Vec<Stmt>,
-}
-
-impl Parse for Test {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let mut is_async = false;
-
-        let attributes = input.call(Attribute::parse_outer)?;
-
-        let lookahead = input.lookahead1();
-        if lookahead.peek(Token![async]) {
-            is_async = true;
-            input.parse::<Token![async]>()?;
-        }
-
-        if lookahead.peek(keyword::it) {
+        } else if lookahead.peek(keyword::it) {
             input.parse::<keyword::it>()?;
+            is_test = true;
         } else if lookahead.peek(keyword::test) {
             input.parse::<keyword::test>()?;
+            is_test = true;
         }
 
         let ident = input.parse::<Ident>()?;
@@ -185,12 +127,19 @@ impl Parse for Test {
 
         let content;
         braced!(content in input);
-        Ok(Test {
+        let mut parsed_content;
+        if is_test {
+            parsed_content = BlockType::Test(content.call(syn::Block::parse_within)?);
+        } else {
+            parsed_content = BlockType::Describe(content.parse::<Scope>()?);
+        }
+
+        Ok(Block {
             attributes,
             is_async,
             ident,
             return_type: None,
-            block: content.call(syn::Block::parse_within)?,
+            content: parsed_content,
         })
     }
 }
--- src/generate.rs
+++ src/generate.rs
@@ -1,34 +1,17 @@
+use crate::block::{Root, BlockType, Block};
 use proc_macro2::TokenStream;
-use crate::block::{Scope, Test, Describe, Block};
 use quote::quote;
 
 pub(crate) trait Generate {
-    fn generate(self, parent: Option<&Scope>) -> TokenStream;
+    fn generate(self, parent: Option<&Block>) -> TokenStream;
 }
 
-impl Generate for Block {
-    fn generate(self, parent: Option<&Scope>) -> TokenStream {
-        match self {
-            Block::Describe(block) => block.generate(parent),
-            Block::Test(block) => block.generate(parent),
-        }
-    }
-}
-
-impl Generate for Scope {
-    fn generate(mut self, parent: Option<&Scope>) -> TokenStream {
-        if let Some(ref parent) = parent {
-            self.before = parent.before
-                .iter()
-                .chain(self.before.iter())
-                .cloned()
-                .collect();
-            self.after.extend(parent.after.clone());
-        }
-
-        let blocks = self.blocks
+impl Generate for Root {
+    fn generate(mut self, parent: Option<&Block>) -> TokenStream {
+        let blocks = self
+            .blocks
             .iter()
-            .map(|block| block.clone().generate(Some(&self)))
+            .map(|block| block.clone().generate(parent))
             .collect::<Vec<TokenStream>>();
 
         quote!(
@@ -37,40 +20,89 @@ impl Generate for Scope {
     }
 }
 
-impl Generate for Describe {
-    fn generate(self, parent: Option<&Scope>) -> TokenStream {
-        let scope = self.scope.generate(parent);
-        let ident = self.ident;
-
-        quote!(
-            #[cfg(test)]
-            mod #ident {
-                use super::*;
-
-                #scope
-            }
-        )
-    }
-}
-
-impl Generate for Test {
-    fn generate(self, parent: Option<&Scope>) -> TokenStream {
-        let ident = self.ident;
-        let block = if let Some(ref parent) = parent {
-            parent.before.iter()
-                .chain(self.block.iter())
-                .chain(parent.after.iter())
+impl Generate for Block {
+    fn generate(mut self, parent: Option<&Block>) -> TokenStream {
+        if let Some(ref parent) = parent {
+            self.attributes = parent
+                .attributes
+                .iter()
+                .chain(self.attributes.iter())
                 .cloned()
-                .collect()
-        } else {
-            self.block
-        };
+                .collect();
 
-        quote!(
-            #[test]
-            fn #ident() {
-                #(#block)*
+            if parent.is_async {
+                self.is_async = true;
             }
-        )
+
+            if let Some(return_type) = parent.return_type {
+                self.return_type = Some(return_type);
+            }
+        }
+
+        match self.content {
+            BlockType::Test(mut content) => {
+                let Block {
+                    attributes,
+                    is_async,
+                    ident,
+                    return_type,
+                    content: _,
+                } = self;
+
+                if let Some(ref parent) = parent {
+                    if let BlockType::Describe(scope) = parent.content {
+                        content = scope
+                            .before
+                            .iter()
+                            .chain(content.iter())
+                            .chain(scope.after.iter())
+                            .cloned()
+                            .collect();
+                    }
+                }
+
+                if is_async {
+                    quote!(
+                        #(#attributes)*
+                        async fn #ident() {
+                            #(#content)*
+                        }
+                    )
+                } else {
+                    quote!(
+                        #(#attributes)*
+                        #[test]
+                        fn #ident() {
+                            #(#content)*
+                        }
+                    )
+                }
+            },
+            BlockType::Describe(mut scope) => {
+                if let Some(ref parent) = parent {
+                    if let BlockType::Describe(parent_scope) = parent.content {
+                        scope.before = parent_scope
+                            .before
+                            .iter()
+                            .chain(scope.before.iter())
+                            .cloned()
+                            .collect();
+                        scope.after.extend(parent_scope.after.clone());
+                    }
+                }
+
+                let ident = self.ident;
+                let scope_blocks = scope.generate(Some(&self));
+
+                quote!(
+                    #[cfg(test)]
+                    mod #ident {
+                        use super::*;
+
+                        #scope_blocks
+                    }
+                )
+            }
+        }
     }
 }
